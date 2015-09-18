6c9f005ec1c5d8056cdd5ff692feb601b6963732
Ceridwen <ceridwenv@gmail.com>
2015-09-17 22:19:29 -0400

    Rename some variables for clarity

--- keyword_only_args.py
+++ keyword_only_args.py
@@ -10,12 +10,16 @@ except ImportError:
 import functools
 import inspect
 
+# This is a placeholder object to distinguish parameters without
+# defaults from parameters have None as their default.
+no_default = object()
 
-def decorator_closure(*included_keywords):
+
+def decorator_factory(*kw_only_parameters):
     """Transforms a function with keyword arguments into one with
     keyword-only arguments.
 
-    Call this decorator as @keyword_only_args() for the default mode,
+    Call this decorator as @decorator_factory() for the default mode,
     which makes all keyword arguments keyword-only, or with the names
     of arguments to make keyword-only.  They must correspond with the
     names of arguments in the decorated function.  It works by
@@ -23,34 +27,44 @@ def decorator_closure(*included_keywords):
     arguments marked as keyword-only from **kws into *args.
 
     Args:
-      *included_keywords: Keyword-only arguments as strings.
+      *kw_only_parameters: Keyword-only arguments as strings.
 
     Returns:
       A decorator that modifies a function so it has keyword-only
       arguments.
 
     """
-    def decorator(func):
-        """Decorator factory, assigns arguments as keyword-only and
+    def decorator(wrapped):
+        """The decorator itself, assigns arguments as keyword-only and
         calculates sets for error checking.
 
         Args:
-          func: The function to decorate.
+          wrapped: The function to decorate.
 
         Returns:
-          A function wrapped so that it has keyword-only arguments. 
-        """
-        positional_args, _, _, defaults = inspect.getargspec(func)
-        args_with_defaults = set(positional_args[len(defaults):])
-        kw_only_args = set(included_keywords) if included_keywords else args_with_defaults.copy()
-        args_defaults = list(zip_longest(reversed(positional_args), reversed(defaults)))
-        args_defaults.reverse()
-        positional_args = set(positional_args)
+          A function wrapped so that it has keyword-only arguments.
 
-        @functools.wraps(func)
+        """
+        # Each Python 3 argument has two independent properties: it is
+        # positional-and-keyword *or* keyword-only, and it has a
+        # default or it doesn't.
+        names, _, _, defaults = inspect.getargspec(wrapped)
+        names_with_defaults = frozenset(names[len(names) - len(defaults):])
+        names_defaults = list(zip_longest(reversed(names), reversed(defaults),
+                                         fillvalue=no_default))
+        names_defaults.reverse()
+        names_defaults = tuple(names_defaults)
+        print(names_defaults)
+        names = frozenset(names)
+        if kw_only_parameters:
+            kw_only_names = frozenset(kw_only_parameters)
+        else:
+            kw_only_names = names_with_defaults.copy()
+
+        @functools.wraps(wrapped)
         def wrapper(*args, **kws):
-            """The decorator itself, checks arguments with set operations, moves
-            args from *args into **kws, and then calls func().
+            """Wrapper function, checks arguments with set operations, moves args
+            from *args into **kws, and then calls wrapped().
 
             Args:
               *args, **kws: The arguments passed to the original function.
@@ -64,35 +78,50 @@ def decorator_closure(*included_keywords):
                 and expected arguments.
 
             """
-            keys = set(kws)
-            # Are all the keyword-only args covered either by a passed
-            # argument or a default?
-            if not kw_only_args <= keys | args_with_defaults:
-                wrong_args(func, args_defaults, kw_only_args - (keys | args_with_defaults), 'keyword-only')
+            kw_names = frozenset(kws)
+            # The keyword-only parameters that are bound to either an
+            # passed-in argument or a default.
+            bound_kw_only_names = kw_names | names_with_defaults
+            # print('\n', bound_kw_only_names)
+
+            # Are all the keyword-only parameters covered either by a
+            # passed argument or a default?
+            if not kw_only_names <= bound_kw_only_names:
+                _wrong_args(wrapped, names_defaults, 
+                           kw_only_names - bound_kw_only_names,
+                           'keyword-only')
             # Are there enough positional args to cover all the
             # arguments not covered by a passed argument or a default?
-            if len(args) < len(positional_args - (keys | args_with_defaults)):
-                wrong_args(func, args_defaults, positional_args - (keys | args_with_defaults), 'positional', len(args))
+            if len(args) < len(names - bound_kw_only_names):
+                _wrong_args(wrapped, names_defaults,
+                           names - bound_kw_only_names,
+                           'positional', len(args))
 
             args = list(args)
-            for index, (name, default) in enumerate(args_defaults):
-                if name in kw_only_args or name in keys & positional_args:
+            # The set of names bound to keyword arguments.
+            bound_kw_names = kw_names & names
+            for index, (name, default) in enumerate(names_defaults):
+                if name in kw_only_names or name in bound_kw_names:
                     args.insert(index, kws.pop(name, default))
-            func(*args, **kws)
+            return wrapped(*args, **kws)
         return wrapper
 
-    def wrong_args(func, args_defaults, missing_args, arg_type, number_of_args=0):
-        """ Raise Python 3-style TypeErrors for missing arguments."""
-        ordered_args = [a for a, _ in args_defaults if a in missing_args]
-        ordered_args = ordered_args[number_of_args:]
-        error_message = ['%s() missing %d required %s argument' % (func.__name__, len(ordered_args), arg_type)]
-        if len(ordered_args) == 1:
-            error_message.append(": '%s'" % ordered_args[0])
-        else:
-            error_message.extend(['s: ', ' '.join("'%s'" % a for a in ordered_args[:-1]), " and '%s'" % ordered_args[-1]])
-        raise TypeError(''.join(error_message))
-
     return decorator
+
+
+def _wrong_args(wrapped, names_defaults, missing_args, arg_type, number_of_args=0):
+    """ Raise Python 3-style TypeErrors for missing arguments."""
+    ordered_args = [a for a, _ in names_defaults if a in missing_args]
+    ordered_args = ordered_args[number_of_args:]
+    error_message = ['%s() missing %d required %s argument' % 
+                     (wrapped.__name__, len(ordered_args), arg_type)]
+    if len(ordered_args) == 1:
+        error_message.append(": '%s'" % ordered_args[0])
+    else:
+        error_message.extend(['s: ', 
+                              ' '.join("'%s'" % a for a in ordered_args[:-1]),
+                              " and '%s'" % ordered_args[-1]])
+    raise TypeError(''.join(error_message))
 
 
 if __name__ == '__main__':
@@ -103,7 +132,15 @@ if __name__ == '__main__':
         except TypeError as e:
             print(e.args[0])
 
-    @keyword_only_args('c')
+    @decorator_factory()
+    def f(a, b, c, d, e='e'):
+        print(a, b, c, d, e)
+
+    test(f)
+    test(f, 0, 1, 2, 3)
+    test(f, 0, 1, 2, 3, 4)
+
+    @decorator_factory('c')
     def f(a, b, c='c', d='d', *args, **kws):
         print(a, b, c, d, args, kws)
 
@@ -114,7 +151,7 @@ if __name__ == '__main__':
     test(f, 0)
     test(f, 0, 1, 2, 3, 4, 5, c='foo', d='bar', e='baz')
 
-    @keyword_only_args('b', 'c')
+    @decorator_factory('b', 'c')
     def f(a, b, c='c', d='d', *args, **kws):
         print(a, b, c, d, args, kws)
 
@@ -126,7 +163,7 @@ if __name__ == '__main__':
     test(f, 0, 1, 2, 3, 4, 5, b='foo', c='bar', e='baz')
 
     class C(object):
-        @keyword_only_args('b', 'c')
+        @decorator_factory('b', 'c')
         def __init__(self, a, b, c='c', d='d', *args, **kws):
             print(a, b, c, d, args, kws)
 
